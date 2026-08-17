686a10e9ed7dd7dfb8e82e88e3e433b7cac42e70
Vikas Pawar <pawar.vikas1111@gmail.com>
2026-08-17 22:12:07 +0530

    Make headless to false

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless: false,
+
   },
 
   /* Configure projects for major browsers */
@@ -39,15 +41,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    /*  {
+       name: 'firefox',
+       use: { ...devices['Desktop Firefox'] },
+     },
+ 
+     {
+       name: 'webkit',
+       use: { ...devices['Desktop Safari'] },
+     }, */
 
     /* Test against mobile viewports. */
     // {
